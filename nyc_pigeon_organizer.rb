40bb297ca29d0879350b33c4bc90842c5c578d4f
Isa Levine <isaacdlevine@gmail.com>
2019-01-25 07:54:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,13 +16,36 @@ end
 
 
 def nyc_pigeon_organizer(data)
-  name_array = name_lister(data)
+
+  # initialize objects
+  pigeon_hash = data
   hash_output = {}
   attribute_array = data.keys
+  name_array = name_lister(data)
+
+  # create keys for each attribute for each Bird Name string
   name_array.each do |pigeon_name|
     hash_output[pigeon_name] = {}
     attribute_array.each do |attribute|
       hash_output[pigeon_name][attribute] = []
     end
   end
+
+  # ITERATE OVER THE NEW HASH FOR EACH NAME, THEN
+  # iterate over original Data hash to find
+  # matches between Bird Name and Attribute
+  # to add to the Attribute(key) array(value)
+  hash_output.each_key do |bird_name|
+    pigeon_hash.each do |attribute_key, attribute_hash|
+      attribute_hash.each do |attr_option_key, pigeon_name_array|
+        if pigeon_array.include?(bird_name)
+          string_temp = attr_option_key.to_s
+          hash_output[bird_name][attribute_key] << string_temp
+        end
+      end
+    end
+
+    # return the hash_output
+    hash_output
+  
 end
