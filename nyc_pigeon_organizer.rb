90fb76415a2248bba93c54f017d0b242c3fff94c
Isa Levine <isaacdlevine@gmail.com>
2019-01-25 07:25:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+def name_lister(data)
+  pigeon_hash = data
+  array_output = []
+  pigeon_hash.each_value do |attribute_hash|
+    attribute_hash.each_value do |name_array|
+      name_array.each do |name_string|
+        array_output << name_string
+      end
+    end
+  end
+  final_array_output = array_output.uniq
+end
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
 end
