25c9dcf1c04414c51ead8b2cd8756a028593672a
Isa Levine <isaacdlevine@gmail.com>
2019-01-25 07:37:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,15 +15,14 @@ def name_lister(data)
 end
 
 
-def attribute_grabber(data)
-  attribute_array = data.keys
-end
-
-
 def nyc_pigeon_organizer(data)
   name_array = name_lister(data)
   hash_output = {}
+  attribute_array = data.keys
   name_array.each do |pigeon_name|
-
+    hash_output[pigeon_name] = {}
+    attribute_array.each do |attribute|
+      hash_output[pigeon_name][attribute] = []
+    end
   end
 end
